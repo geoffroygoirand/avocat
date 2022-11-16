5d11a600fccc301bc2c7fda694c5a04a2138c55c
geoffroy goirand <geoffroy.goirand@gmail.com>
2022-11-16 22:10:00 +0100

    first commit14

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,21 @@ import NavbarSite from './Nav.js';
 import Formulaire from './Form.js'; 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import avocat from './avocat.jpg'
-
-
+import ReactDOM from "react-dom/client";
+import {  Routes, Route } from "react-router-dom";
+import Home from "./Home.js"
+import Dossiers from "./Dossiers.js"
 
 
 function App() {
   return (
   
       <div className="App">
-      
-      <NavbarSite />
-      <div className="Container">
-      <img src={avocat} alt="avocat" />
-      </div>
-      <Formulaire />
+     
+      <Routes>
+          <Route path ="/" element= {<Home />} />
+          <Route path ="/Dossiers" element= {<Dossiers />} />
+      </Routes>
         
     </div>
   );
---
+++ my-app/src/Home.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import NavbarSite from './Nav.js'; 
+import Formulaire from './Form.js'; 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import avocat from './avocat.jpg'
+
+
+
+function Home() {
+  return (
+  
+      <div className="App">
+       
+      <NavbarSite />
+      <div className="Container">
+      <img src={avocat} alt="avocat" />
+      </div>
+      <Formulaire />
+        
+    </div>
+  );
+}
+
+export default Home;
